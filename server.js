8f0b9b6c2948e7910f5722af701bf59f924efa17
telthhealth-cloud <telthhealth@gmail.com>
2026-05-07 17:18:32 +0530

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express=require('express')
 const log=require('express-dev-logger')
+const cors=require('cors')
 // server.js — add these two lines anywhere after you create your app
 const app=express()
 
 app.use(express.json())
 app.use(log())
+app.use(cors())
 const { registerMonitorRoutes } = require('./domainCheck');
 registerMonitorRoutes(app);
 
